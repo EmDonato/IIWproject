e124c6e9a4d256bb512d5a81a0d3627c7d3eb551
EmDonato <mcf.donato@gmail.com>
2021-11-30 18:07:33 +0100

    Update packet.h

--- Fabrizio/packet.h
+++ Fabrizio/packet.h
@@ -1,7 +1,7 @@
 #define MAXLINE     1024
 #define SERV_PORT 5193
 #define BACKLOG 10
-#define MAXLINE 1024
+#define MAXLINE 1024  // lo diminuirei
 #define N 10
 #define NEWSERV_PORT 519
 
@@ -20,7 +20,12 @@
 	char data[MAXLINE];
 }packet;
 
-
+typedef struct packetsend{ // struttura dati per sapere i pacchetti mandati
+	int32_t seqnumb;
+	int32_t acknumb;
+	bool overwritable;
+	int position;
+}packetsend;
 
 
 void shutout(int sig);
@@ -36,3 +41,8 @@ int foundAndDetroy(struct sockaddr_in *addr,int x);
 
 
 int found(struct sockaddr_in *addr,int x);
+
+
+int reorder(packetsend array[], int size);
+
+void printArray(packetsend array[], int size);
