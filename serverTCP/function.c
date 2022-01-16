1c763e2a7ce26b3db18c158db1d201a82281b3f6
EmDonato <mcf.donato@gmail.com>
2022-01-16 17:58:44 +0100

    Update function.c

--- serverTCP/function.c
+++ serverTCP/function.c
@@ -17,8 +17,7 @@
 #include <stdbool.h>
 #include <dirent.h>
 #include "packet.h"
-#define SERV_PORT   5193 
-#define SIZE  128 
+ 
 
 extern key_t semkey;
 extern int sockfd, connsd;
@@ -33,6 +32,7 @@ extern bool ausiliarSyn_rcvd[N];
 extern int semID;
 int32_t snprc;
 int32_t anprc;
+
 int found(struct sockaddr_in *addr,int x){
 	
 	//funzione che ricerca un posto nella coda per stabilire una connessione
@@ -59,7 +59,6 @@ int found(struct sockaddr_in *addr,int x){
 				
 			}
 		}
-	printf("\n\nla posizione trovata e la numero: %d\n\n",position);
 		
 	}
 	else{
@@ -111,11 +110,7 @@ int foundAndDetroy(struct sockaddr_in *addr,int x){
 			gateAccepted[x].sockID=gateSyn_rcvd[i].sockID;
 			gateAccepted[x].address=gateSyn_rcvd[i].address;
 			memset((void *)&gateSyn_rcvd[i],0,sizeof(struct gate));			
-			
-			
-			
-			
-			
+
 			return 1;
 		}
 	}
@@ -126,187 +121,135 @@ int foundAndDetroy(struct sockaddr_in *addr,int x){
 
 void *mylisten(void *arg){
 	
-	printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
+	/* funzione che simula la funzione di libreria listen(), si occupa dell'handshaking tra il server 
+	e il client, in base a due a i pacchetti che arrivano e lo stato delle stutture di controllo per la connessione
+	vengono gestiti i client, a ogni cliente viene associata una nuova porta libera per poi successivamente permettere 
+	la connessione sicura 	*/
+	
 	fflush(stdout);
+	srand (time(NULL));	
 	int32_t r = rand(); //numero della sequenza
-	
+	int numb;
+	float probaus;
+	srand (time(NULL));		
+	numb = rand();
+	probaus = (double)numb /(double)RAND_MAX;
 	struct sembuf buf[1];
-	
-	//prima cosa pulisco gli array dove conterro le connessioni
-	
 	memset((void *)&accepted, 0, BACKLOG*sizeof(struct sockaddr_in));
 	memset((void *)&syn_rcvd, 0, N*sizeof(struct sockaddr_in));
 	memset((void *)&gateAccepted, 0, BACKLOG*sizeof(struct gate));
 	memset((void *)&gateSyn_rcvd, 0, N*sizeof(struct gate));	
 	char nameint[6];
-	
-	
 	int i=0,len=sizeof(struct sockaddr_in);
 	int foundresult;
-	struct sockaddr_in addr,newAddr; //struttura dove verra consegnato l informazione sul mittente
-	packet pacrcv,pacchetto;	 //struttura dove verra consegnato il pacchetto, pacchetto e quello che mandi
+	struct sockaddr_in addr,newAddr; 
+	packet pacrcv,pacchetto;	 
 	srand(time(NULL));
 	memset((void *)&pacrcv, 0, sizeof(packet));
 	memset((void *)&pacchetto, 0, sizeof(packet));
-	//creo il semaforo per la connect
- 
-	// vriabili che conterranno Id socket e numero di porta 
 	int ids,portaus, lenname = sizeof(struct sockaddr);
 	
  
 	while(1){
-		//ricevo un pacchetto
 		if((recvfrom(sockfd,(void *)&pacrcv, sizeof(packet), 0 ,(struct sockaddr *) &addr,&len )) < 0) {
 			perror("errore in recvfrom");
 			exit(1);
 		}
-		
-		//controllo il pacchetto che e arrivato
-		printf("dentro il while del my listen\n\n\n\n ");
+		if(probaus<=PROB){
 
-		printf("il pacchetto arrivato e:\n numb:%d\nack:%d\nack:%d\nsyn:%d\n il campo data%s\n ",pacrcv.seqnumb,pacrcv.acknumb,pacrcv.flags.ack,pacrcv.flags.syn,pacrcv.data);
-		
-		
-		//checko il tipo di messaggio che ricevo e da li controllo in che array controllare
-		if(pacrcv.flags.ack==0 && pacrcv.flags.syn==1){
-			//cerco     se lo trovo ora non sto facendo nulla
-			if((foundresult=found(&addr,0)) > -1){
-				srand(time(NULL));
-				printf("la foundresult e di %d\n\n",foundresult);
-				syn_rcvd[foundresult]=addr; //se non lo trovo lo aggiungo
-				//criamo il socket 
-				memset((void *)&newAddr, 0, sizeof(struct sockaddr_in));
-				if ((ids = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
-					perror("errore in socket");
-					exit(1); 
+		}
+		else{
+
+			if(pacrcv.flags.ack==0 && pacrcv.flags.syn==1){
+				if((foundresult=found(&addr,0)) > -1){
+					srand(time(NULL));
+					syn_rcvd[foundresult]=addr; //se non lo trovo lo aggiungo
+					//criamo il socket 
+					memset((void *)&newAddr, 0, sizeof(struct sockaddr_in));
+					if ((ids = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+						perror("errore in socket");
+						exit(1); 
+					}
+					newAddr.sin_family = AF_INET;
+					newAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* il server accetta pacchetti su una qualunque delle sue interfacce di rete */
+					newAddr.sin_port = htons(0); /* numero di porta del server */				
+					if (bind(ids, (struct sockaddr *)&newAddr, sizeof(struct sockaddr_in)) < 0) {
+						perror("errore in bind");
+						exit(1);
+					}
+					gateSyn_rcvd[foundresult].sockID =ids;
+					getsockname(ids, (struct sockaddr *)&newAddr,&lenname);
+					gateSyn_rcvd[foundresult].address = newAddr.sin_port;
+					ausiliarSyn_rcvd[foundresult] = true;
+					pacchetto.seqnumb = r; //assegno un numero casuale come numero sequenza
+					pacchetto.acknumb = pacrcv.seqnumb + 1; //richiedo il byte successivo
+					pacchetto.flags.ack = 1;
+					pacchetto.flags.rst = 0;
+					pacchetto.flags.syn = 1;
+					pacchetto.flags.fin = 0;
+					sprintf(nameint,"%d",gateSyn_rcvd[foundresult].address);
+					strcpy(pacchetto.data,nameint);
+
+					if (sendto(sockfd, (const void *)&pacchetto, sizeof(packet), 0, (struct sockaddr *)&addr, len ) < 0) {
+						perror("errore in sendto 1");
+						exit(1); // attemzione
+					}
+					
 				}
-				newAddr.sin_family = AF_INET;
-				newAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* il server accetta pacchetti su una qualunque delle sue interfacce di rete */
-				newAddr.sin_port = htons(0); /* numero di porta del server */				
-				if (bind(ids, (struct sockaddr *)&newAddr, sizeof(struct sockaddr_in)) < 0) {
-					perror("errore in bind");
-					exit(1);
+				else if(foundresult == -2){
+
 				}
-				gateSyn_rcvd[foundresult].sockID =ids;
-				getsockname(ids, (struct sockaddr *)&newAddr,&lenname);
-				gateSyn_rcvd[foundresult].address = newAddr.sin_port;
-				//fine
-				ausiliarSyn_rcvd[foundresult] = true;
-				pacchetto.seqnumb = r; //assegno un numero casuale come numero sequenza
-				pacchetto.acknumb = pacrcv.seqnumb + 1; //richiedo il byte successivo
-				pacchetto.flags.headlen = 10;		//**************cambiare size of
-				pacchetto.flags.ack = 1;
-				pacchetto.flags.rst = 0;
-				pacchetto.flags.syn = 1;
-				pacchetto.flags.fin = 0;
+				else if(foundresult == -1){
+					if (sendto(sockfd, (const void *)&pacchetto, sizeof(packet), 0, (struct sockaddr *)&addr, len ) < 0) {
+						perror("errore in sendto 1");
+						exit(1); // attemzione
+						}
+				}
+			}
+
+			else if(pacrcv.flags.ack==1 && pacrcv.flags.syn==0){    // controllare il numero dell ack
+				if((foundresult=found(&addr,1)) > -1){
+					if(foundAndDetroy(&addr, foundresult)){
+						buf[0].sem_num = 0;
+						buf[0].sem_op = 1;
+						buf[0].sem_flg = 0;
+						semop(semID, buf, 1);
+						((int32_t *)arg)[0] = pacrcv.seqnumb;
+						((int32_t *)arg)[1] = pacrcv.acknumb;
+					
+					}
+					else{
+						
+					}
+				}
+				else if(foundresult == 0){
+				}
 				
-				sprintf(nameint,"%d",gateSyn_rcvd[foundresult].address);
-				strcpy(pacchetto.data,nameint);
-				printf("il pacchetto da mandare la prima volta:\n %d \n %d \n %d \n %d\n %s ",pacchetto.seqnumb,pacchetto.acknumb,pacchetto.flags.ack,pacchetto.flags.syn,pacchetto.data);
+			}
 
-				if (sendto(sockfd, (const void *)&pacchetto, sizeof(packet), 0, (struct sockaddr *)&addr, len ) < 0) {
-					perror("errore in sendto 1");
-					exit(1); // attemzione
-				}
-				
-			}
-			else if(foundresult == -2){
-				printf("non piu spazio nella syn_rcvd\n");
-			}
-			else if(foundresult == -1){
-				printf("gia presenten nella syn_rcvd\n");
-			}
 		}
-
-		else if(pacrcv.flags.ack==1 && pacrcv.flags.syn==0){    // controllare il numero dell ack
-			//trovare nella lista e toglierlo e metterlo nell altra list se c'e un posto libero
-			if((foundresult=found(&addr,1)) > -1){
-				if(foundAndDetroy(&addr, foundresult)){
-					//aumento il semaforo di uno
-					printf("fatto il found and destroy\n");
-					buf[0].sem_num = 0;
-					buf[0].sem_op = 1;
-					buf[0].sem_flg = 0;
-					semop(semID, buf, 1);
-					((int32_t *)arg)[0] = pacrcv.seqnumb;
-					((int32_t *)arg)[1] = pacrcv.acknumb;
-					printf("arg0 %d, aarg1%d \n\n",((int32_t *)arg)[0], ((int32_t *)arg)[1]);
-					printf("\n\n********************dato il token del semaforo nell mylisten***************\n\n");
-					printf("completato l handshake\n");
-				}
-				else{
-					printf("non presente nella lista dei syn\n");
-				}
-			}
-			else if(foundresult == 0){
-				printf("non piu spazio nella accepted\n");
-			}
-			
-	    }
-		
-		
-		// printa ogni volta il contenuto debug  CANCELLARE
-		
-		i = 0;
-		printf("contenuto della lista delle connessioni stabilite: \n");
-		for(i=0;i<BACKLOG;i++){
-			printf("porta: %d\nIP: %d \n ",accepted[i].sin_port, accepted[i].sin_addr.s_addr);
-			
-		
-		}
-
-		i = 0;
-		printf("\n\ncontenuto della lista delle connessioni in attesa di syn ack: \n");		
-		for(i=0;i<N;i++){
-			printf("porta: %d\nIP: %d\n ",syn_rcvd[i].sin_port, syn_rcvd[i].sin_addr.s_addr);
-			
-		
-		}
-		i = 0;
-		printf("\n\ncontenuto della lista dei sochet e degli indirizzi syn ack: \n");		
-		for(i=0;i<N;i++){
-			printf("porta: %d\n socket: %d\n ",gateSyn_rcvd[i].address, gateSyn_rcvd[i].sockID);
-			
-		
-		}		
-		printf("\n\ncontenuto delle posizioni dell arrey delle accettate: \n");		
-		for(i=0;i<N;i++){
-			printf("%d ",ausiliarAccepted[i]);
-			
-		
-		}	
-		printf("\n\ncontenuto delle posizioni dell arrey delle syn_rcvd: \n");		
-		for(i=0;i<N;i++){
-			printf("%d ",ausiliarSyn_rcvd[i]);
-			
-		
-		}
-		printf("\n\n");
 	}
-
 }
 
 
 
 int myaccept(int sockfd, struct sockaddr_in *addr1, socklen_t *addrlen){
 	
+/* 	fuzione che simula la accept(), viene preso il prim client
+	che ha suoerato l'handshake, nella pila delle accepted, viene restituito 
+	l'identificativo del sockt e viene,tramite il puntatore, restituito le info sul client  */
+	
 	struct sockaddr_in addrpers;
 	memset((void *)&addrpers, 0, sizeof(addrpers));
-
 	int i = 0, IDsock;
 	struct sembuf buf[1];
-	printf("\n\n********************in attesa del token del semaforo nell accept***************\n\n");
 	buf[0].sem_num = 0;
 	buf[0].sem_op = -1;
 	buf[0].sem_flg = 0;
 	semop(semID, buf, 1);
-	printf("\n\n********************preso il token del semaforo nell accept***************\n\n");
 	while(ausiliarAccepted[i] == false){
-		printf("in while per trovare la posizione\n\n");
 		i++;
 	}
-	printf("l aposizione del vero sta nella posizione %d\n\n\n",i);
-	printf("\n\n********************trovato la connessione in posizione: %d***************\n\n",i);
 	addr1->sin_family = accepted[i].sin_family;
 	addr1->sin_port = accepted[i].sin_port;
 	addr1->sin_addr.s_addr = accepted[i].sin_addr.s_addr;
@@ -322,52 +265,19 @@ int myaccept(int sockfd, struct sockaddr_in *addr1, socklen_t *addrlen){
 
 void shutout(int sig){
 	
-	printf("\n\n********************entrato nel gestore del segnale SIGINT***************\n\n");
+	//gestore per il segnale SIGINT, si occupa della chiusura del server 
+	printf("\n\n**************************** SERVER NON ATTIVO ****************************\n\n");
 	semctl(semID, -1, IPC_RMID, 0);
-	exit(1);
+	raise(SIGKILL);
 	
-}
-
-/* int reorder(packetsend array[], int size){
-	
-  //it's a bubblesort
-  
-  packetsend temp;
-  // loop to access each array element
-  for (int step = 0; step < size - 1; ++step) {
-      
-    // loop to compare array elements
-		for (int i = 0; i < size - step - 1; ++i) {
-		  
-		  // compare two adjacent elements
-		  // change > to < to sort in descending order
-		  if (array[i].seqnumb > array[i + 1].seqnumb) {
-			
-			// swapping occurs if elements
-			// are not in the intended order
-			memcpy((void *)&temp,(void *)&array[i],sizeof(packetsend));
-			memcpy((void *)&array[i],(void *)&array[i + 1],sizeof(packetsend));
-			memcpy((void *)&array[i + 1],(void *)&temp,sizeof(packetsend));
-
-			}
-		}
-    }
-	return 0;
-} */
-
-
-
-void printArray(packetsend array[], int size) {
-  for (int i = 0; i < size; ++i) {
-    printf("in posizione %d\n seqnumb = %d\n acknumb = %d\n overwritable = %d\n position = %d\n", i,  array[i].seqnumb, array[i].acknumb, array[i].overwritable,array[i].position);
-	printf("\n\n");
-  }
-  printf("\n");
 }
 
 
 
 int strswitch(char *str){
+	
+	//funzione che si occupa dell hashing tra il comando digitato e il giusto intero  
+	
 	int i;
 	if(strcmp(str,"list") == 0){
 		i = 0;
@@ -381,13 +291,15 @@ int strswitch(char *str){
 	else if(strcmp(str,"get") == 0){
 		i = 3;
 	}	
-	printf("dentro la funzione compare la i e %d",i);
 	return i;
 }
 
 
 
 void makePacket(packet *p,int32_t sn,int32_t an, int  flg){
+	
+	//funzione che crea il pacchetto dati in risposta alla richiesta di comando, 
+	//se flg == 0 allore tutto e andato a buon fine, flg == 1 errore generico
 	
 	if(flg == 0){	
 		p->seqnumb = sn;
@@ -399,19 +311,15 @@ void makePacket(packet *p,int32_t sn,int32_t an, int  flg){
 		p->seqnumb = sn;
 		p->acknumb = an;
 		p->flags.ack = 1;
-		strcpy(p->data,"ERROR");
-		
-		
-		
-		
+		strcpy(p->data,"ERROR");		
 	}
-	
-	
-	
 }
 
 
 int ls(FILE *f) { 
+
+	//funzione che stampa il direttorio corrente in un file 
+	
 	struct dirent **dirent; int n = 0;
 
 	if ((n = scandir(".", &dirent, NULL, alphasort)) < 0) { 
